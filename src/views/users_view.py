86757e3b3ccf0c7d96633cb39c37518c53475ee6
smwatertd <korablev.danil.smtd@mail.ru>
2023-10-13 16:43:14 +0500

    update users_view: register user_register_error_handler

--- src/views/users_view.py
+++ src/views/users_view.py
@@ -1,5 +1,8 @@
 from core.extensions import db
 
+from exceptions import UserRegisterError
+from exceptions.handlers import user_register_error_handler
+
 from flask import request
 from flask.blueprints import Blueprint
 
@@ -9,6 +12,9 @@ from services import create_user
 
 
 users_view = Blueprint('users', __name__)
+
+
+users_view.register_error_handler(UserRegisterError, user_register_error_handler)
 
 
 @users_view.route('/users', methods=['POST'])
