9a71ed300dffd0bbcc248b0ddd4497957f6b3795
smwatertd <korablev.danil.smtd@mail.ru>
2023-10-13 16:37:58 +0500

    update SQLAlchemyUserRepository: add is_user_exists method

--- src/repositories/sqlalchemy/sqlalchemy_user_repository.py
+++ src/repositories/sqlalchemy/sqlalchemy_user_repository.py
@@ -8,5 +8,8 @@ from sqlalchemy.orm import Session
 
 
 class SQLAlchemyUserRepository(UserRepository):
+    def is_user_exists(self, session: Session, **kwargs: dict) -> bool:
+        return session.query(User).filter_by(**kwargs).first() is not None
+
     def create_user(self, session: Session, user: CreateUserSchema) -> None:
         session.add(User(**user.model_dump()))
